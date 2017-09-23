85568d813601a80af3ec6335b8c3bc9d6680c977
Miguel Duarte <Meiogordo@users.noreply.github.com>
2017-09-24 00:39:06 +0100

    Uploading all the stuff

---
+++ AEDA-TPTemplate/cmake-build-debug/CMakeFiles/AEDA_TPTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AEDA_TPTemplate.dir/src/main.cpp.o"
+  "AEDA_TPTemplate.pdb"
+  "AEDA_TPTemplate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AEDA_TPTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
